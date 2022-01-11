05377b17472cf399ae9a034404ca3bafbbc185c7
Andy Maleh <andy.am@gmail.com>
2022-01-11 10:03:08 -0500

    Another Opal example

--- app/assets/javascript/application.js.rb
+++ app/assets/javascript/application.js.rb
@@ -1,12 +1,26 @@
 require "opal"
 
 # Uncomment the following to print out you're hello-world with Opal:
-
-  puts "hello world!"
-
+#
+#   puts "hello world!"
+#
 # The following will append a hello-world to your <body> element:
+#
+#   require "native"
+#   $$[:document].addEventListener :DOMContentLoaded do
+#     $$[:document][:body][:innerHTML] = '<h2>Hello World!</h2>'
+#   end
 
-  require "native"
-  $$[:document].addEventListener :DOMContentLoaded do
-    $$[:document][:body][:innerHTML] = '<h2>Hello World!</h2>'
+puts "hello world!"
+
+require "native"
+$$.document.addEventListener(:DOMContentLoaded) do
+  $$.document.body.innerHTML = '<h2>Hello, World!</h2><button>Update Greeting</button>'
+
+  button = $$.document.getElementsByTagName('button')[0]
+
+  button.addEventListener(:click) do
+    greeting = $$.document.getElementsByTagName('h2')[0]
+    greeting.innerHTML = 'Howdy, Partner!'
   end
+end
